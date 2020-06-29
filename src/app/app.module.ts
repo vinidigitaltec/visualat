bfffada0786278831710d974e6b50a0a7a274459
vinidigitaltec <vinicius.andrade.castro@gmail.com@gmail.com>
2020-06-29 16:00:54 -0300

    atividade feita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { AlertsComponent } from './alerts/alerts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadcrumbComponent,
+    AlertsComponent
   ],
   imports: [
     BrowserModule
